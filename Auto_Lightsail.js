3e9ec5a034f3969ddea20d43e103e82685ec7a66
Au0727 <125126631+Au0727@users.noreply.github.com>
2024-05-01 16:20:34 +0800

    Create Auto_Lightsail.js

---
+++ Auto_Lightsail.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         AutoLightsail
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-01
+// @description  Read Lightsail books automatically without triggering the anti-bot check!!!
+// @author       Au0727
+// @match        https://lightsailed.com/*
+// @icon         https://lightsailed.com/school/literacy/assets/images-school/logo.svg
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    function clickButton() {
+  var button = document.querySelector('button.reader-button-next.btn');
+  if (button) {
+    button.click();
+  var button1 = document.querySelector('button.reader-button-prev.btn');
+  if (button1) {
+      button1.click();
+  }
+  }
+}
+
+
+setInterval(clickButton, 30000);
+
+})();
